66da2006a3a695080a4af2889c3ec6f3c5753090
Gabriel Carmona <gabrielgcarmona@gmail.com>
2021-03-23 22:44:01 -0300

    Usando blinka

--- scripts/adc_i2c.py
+++ scripts/adc_i2c.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 import rospy
-import Adafruit_ADS1x15
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 from std_msgs.msg import Int32
 
-global adc
-adc = Adafruit_ADS1x15.ADS1015()
+
 
 def main():
-    global adc
+    
+
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ads = ADS.ADS1015(i2c)
+    channel = AnalogIn(ads, ADS.P0)
 
     pub = rospy.Publisher('dados', Int32, queue_size=10)
-    rospy.init_node('adc', anonymous=True)
+    rospy.init_node('adc', anonymous=False)
     rate = rospy.Rate(1600)
     
-    adc.start_adc(0, gain=1)
+    
     while not rospy.is_shutdown():
-        dados = adc.get_last_result()
+        dados = channel.voltage *1000
         pub.publish(Int32(dados))
         rate.sleep()
 if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
-        adc.stop_adc()
+        pass
